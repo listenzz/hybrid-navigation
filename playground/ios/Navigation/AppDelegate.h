cd53ed125c114f7a5460e052c166d5563ad9e686
Listen <listenzz@163.com>
2017-11-18 17:58:06 +0800

    ios initial playground

---
+++ playground/ios/Navigation/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Navigation
+//
+//  Created by Listen on 2017/11/18.
+//  Copyright © 2017年 Listen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ playground/ios/Navigation/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Navigation
+//
+//  Created by Listen on 2017/11/18.
+//  Copyright © 2017年 Listen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
