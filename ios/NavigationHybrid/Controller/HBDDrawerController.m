fdccc03f148960053a3a30978b4c833fa28c279e
Listen <listenzz@163.com>
2018-08-23 17:43:21 +0800

    do not forward appearance transition when toggle menu

--- ios/NavigationHybrid/Controller/HBDDrawerController.m
+++ ios/NavigationHybrid/Controller/HBDDrawerController.m
@@ -174,7 +174,7 @@
     UIViewController *menu = self.menuController;
     float menuWidth = [self menuWidth];
     
-    [self.contentController beginAppearanceTransition:NO animated:YES];
+    // [self.contentController beginAppearanceTransition:NO animated:YES];
     [self.menuController beginAppearanceTransition:YES animated:YES];
     
     [UIView animateWithDuration:0.2 delay:0. options:UIViewAnimationOptionCurveEaseOut animations:^{
@@ -182,7 +182,7 @@
         self.menuDimmingView.alpha = 0.5;
         menu.view.frame = CGRectMake(0, 0, menuWidth, CGRectGetHeight(self.view.bounds));
     } completion:^(BOOL finished) {
-        [self.contentController endAppearanceTransition];
+        // [self.contentController endAppearanceTransition];
         [self.menuController endAppearanceTransition];
     }];
 }
@@ -211,7 +211,7 @@
     CGFloat duration = ( 1- (dx + menuWidth)/menuWidth ) * 0.2;
     
     [self.menuController beginAppearanceTransition:NO animated:YES];
-    [self.contentController beginAppearanceTransition:YES animated:YES];
+    // [self.contentController beginAppearanceTransition:YES animated:YES];
    
     [UIView animateWithDuration:duration delay:0. options:UIViewAnimationOptionCurveEaseIn animations:^{
         self.menuController.view.frame = rect;
@@ -224,7 +224,7 @@
         self.menuHolderView = nil;
         self.menuOpened = NO;
         [self.menuController endAppearanceTransition];
-        [self.contentController endAppearanceTransition];
+        // [self.contentController endAppearanceTransition];
     }];
 }
 
@@ -233,13 +233,13 @@
     CGFloat dx = 0 - CGRectGetMinX(self.menuController.view.frame);
     CGRect rect = CGRectOffset(self.menuController.view.frame, dx, 0);
     CGFloat duration = (dx/width) * 0.2;
-    [self.contentController beginAppearanceTransition:NO animated:YES];
+    // [self.contentController beginAppearanceTransition:NO animated:YES];
     [self.menuController beginAppearanceTransition:YES animated:YES];
     [UIView animateWithDuration:duration delay:0.0 options:UIViewAnimationOptionCurveEaseIn animations:^{
         self.menuController.view.frame = rect;
         self.menuDimmingView.alpha = 0.5;
     } completion:^(BOOL finished) {
-        [self.contentController endAppearanceTransition];
+        // [self.contentController endAppearanceTransition];
         [self.menuController endAppearanceTransition];
     }];
 }
