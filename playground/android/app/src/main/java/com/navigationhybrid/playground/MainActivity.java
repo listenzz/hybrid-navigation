ae6c9d14e49b102933c6d8dffbeb529713a12254
Listen <listenzz@163.com>
2018-09-30 15:15:08 +0800

    demo how to use `handleBackPressed`

--- playground/android/app/src/main/java/com/navigationhybrid/playground/MainActivity.java
+++ playground/android/app/src/main/java/com/navigationhybrid/playground/MainActivity.java
@@ -4,5 +4,11 @@ import com.navigationhybrid.ReactAppCompatActivity;
 
 public class MainActivity extends ReactAppCompatActivity {
 
+    @Override
+    protected boolean handleBackPressed() {
+        // 按返回键并不真正退出 APP，而是把它移到后台
+        moveTaskToBack(false);
+        return true;
+    }
 
 }
