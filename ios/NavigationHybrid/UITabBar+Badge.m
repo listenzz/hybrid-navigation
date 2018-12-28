6c99ac9faa67aacb2ddf9776fe1debb6f845c7c1
Listen <listenzz@163.com>
2018-12-28 17:21:43 +0800

    adjust badge position

--- ios/NavigationHybrid/UITabBar+Badge.m
+++ ios/NavigationHybrid/UITabBar+Badge.m
@@ -13,7 +13,7 @@
     [self hideRedPointAtIndex:index];
     //label为小红点，并设置label属性
     UILabel *label = [[UILabel alloc]init];
-    label.tag = 1000+index;
+    label.tag = 1000 + index;
     label.layer.cornerRadius = 5;
     label.clipsToBounds = YES;
     if (@available(iOS 10.0, *)) {
@@ -24,10 +24,10 @@
     CGRect tabFrame = self.frame;
    
     //计算小红点的X值，根据第index控制器，小红点在每个tabbar按钮的中部偏移0.1，即是每个按钮宽度的0.6倍
-    CGFloat percentX = (index+0.6);
-    CGFloat tabBarButtonW = CGRectGetWidth(tabFrame)/self.items.count;
+    CGFloat percentX = (index + 0.6);
+    CGFloat tabBarButtonW = CGRectGetWidth(tabFrame) / self.items.count;
     CGFloat x = percentX*tabBarButtonW;
-    CGFloat y = 0.1*CGRectGetHeight(tabFrame);
+    CGFloat y = 0.05 * CGRectGetHeight(tabFrame);
     //10为小红点的高度和宽度
     label.frame = CGRectMake(x, y, 10, 10);
     
@@ -37,8 +37,8 @@
 }
 
 - (void)hideRedPointAtIndex:(NSInteger)index {
-    for (UIView*subView in self.subviews) {
-        if (subView.tag == 1000+index) {
+    for (UIView *subView in self.subviews) {
+        if (subView.tag == 1000 + index) {
             [subView removeFromSuperview];
         }
     }
