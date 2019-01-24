82b6002b00da922d4d527f6c42861fa9e4a6285c
Listen <listenzz@163.com>
2019-01-24 20:00:07 +0800

    code format

--- android/src/main/java/com/navigationhybrid/ReactTabBarProvider.java
+++ android/src/main/java/com/navigationhybrid/ReactTabBarProvider.java
@@ -9,7 +9,6 @@ import android.support.annotation.Nullable;
 import android.util.Pair;
 import android.view.Gravity;
 import android.view.View;
-import android.view.ViewGroup;
 import android.widget.FrameLayout;
 
 import com.facebook.react.ReactInstanceManager;
@@ -277,8 +276,8 @@ public class ReactTabBarProvider implements TabBarProvider {
 
     private final ReactBridgeManager bridgeManager = ReactBridgeManager.get();
 
-    public @NonNull
-    ReactBridgeManager getReactBridgeManager() {
+    @NonNull
+    public ReactBridgeManager getReactBridgeManager() {
         return bridgeManager;
     }
 }
