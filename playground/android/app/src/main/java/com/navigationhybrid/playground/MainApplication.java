186d1e448115435789d88484512ce2da86b7d12c
Listen <listenzz@163.com>
2019-03-04 16:02:31 +0800

    Revert "add matrix"
    This reverts commit bf7fa2b2783451bb8a46feb6bd77224116c2e62a.

--- playground/android/app/src/main/java/com/navigationhybrid/playground/MainApplication.java
+++ playground/android/app/src/main/java/com/navigationhybrid/playground/MainApplication.java
@@ -12,14 +12,6 @@ import com.navigationhybrid.NavigationHybridPackage;
 import com.navigationhybrid.ReactBridgeManager;
 import com.oblador.vectoricons.VectorIconsPackage;
 import com.taihua.hud.HUDReactPackage;
-import com.tencent.matrix.Matrix;
-import com.tencent.matrix.iocanary.IOCanaryPlugin;
-import com.tencent.matrix.iocanary.config.IOConfig;
-import com.tencent.matrix.resource.ResourcePlugin;
-import com.tencent.matrix.resource.config.ResourceConfig;
-import com.tencent.matrix.trace.TracePlugin;
-import com.tencent.matrix.trace.config.TraceConfig;
-import com.tencent.matrix.util.MatrixLog;
 
 import java.util.Arrays;
 import java.util.List;
@@ -30,8 +22,6 @@ import java.util.List;
  */
 
 public class MainApplication extends MultiDexApplication implements ReactApplication{
-
-    private static final String TAG = "Matrix.Application";
 
     private final ReactNativeHost mReactNativeHost = new HybridReactNativeHost(this) {
         @Override
@@ -63,58 +53,6 @@ public class MainApplication extends MultiDexApplication implements ReactApplica
     @Override
     public void onCreate() {
         super.onCreate();
-
-        DynamicConfigImplDemo dynamicConfig = new DynamicConfigImplDemo();
-        boolean matrixEnable = dynamicConfig.isMatrixEnable();
-        boolean fpsEnable = dynamicConfig.isFPSEnable();
-        boolean traceEnable = dynamicConfig.isTraceEnable();
-
-        MatrixLog.i(TAG, "MatrixApplication.onCreate");
-
-        Matrix.Builder builder = new Matrix.Builder(this);
-        builder.patchListener(new TestPluginListener(this));
-
-        //trace
-        TraceConfig traceConfig = new TraceConfig.Builder()
-                .dynamicConfig(dynamicConfig)
-                .enableFPS(fpsEnable)
-                .enableMethodTrace(traceEnable)
-                .enableStartUp(traceEnable)
-                .splashActivity("com.navigationhybrid.playground.SplashActivity")
-                .build();
-
-        TracePlugin tracePlugin = (new TracePlugin(traceConfig));
-        builder.plugin(tracePlugin);
-
-        if (matrixEnable) {
-
-            //resource
-            builder.plugin(new ResourcePlugin(new ResourceConfig.Builder()
-                    .dynamicConfig(dynamicConfig)
-                    .setDumpHprof(false)
-                    .setDetectDebuger(true)     //only set true when in sample, not in your app
-                    .build()));
-            ResourcePlugin.activityLeakFixer(this);
-
-            //io
-            IOCanaryPlugin ioCanaryPlugin = new IOCanaryPlugin(new IOConfig.Builder()
-                    .dynamicConfig(dynamicConfig)
-                    .build());
-            builder.plugin(ioCanaryPlugin);
-
-        }
-
-        Matrix.init(builder.build());
-
-        //start only startup tracer, close other tracer.
-        tracePlugin.start();
-        //only stop at sample app, in your app do not call onDestroy
-        tracePlugin.getFPSTracer().onDestroy();
-
-        MatrixLog.i("Matrix.HackCallback", "end:%s", System.currentTimeMillis());
-
-
-
         SoLoader.init(this, false);
 
         ReactBridgeManager bridgeManager = ReactBridgeManager.get();
