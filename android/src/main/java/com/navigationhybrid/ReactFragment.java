8f714f92512874aeedc5376ff88e157861dbaadf
Listen <listenzz@163.com>
2019-03-15 10:38:33 +0800

    start react app immediately if optimization is not enabled

--- android/src/main/java/com/navigationhybrid/ReactFragment.java
+++ android/src/main/java/com/navigationhybrid/ReactFragment.java
@@ -66,7 +66,7 @@ public class ReactFragment extends HybridFragment implements ReactRootViewHolder
             reactRootViewHolder.setVisibilityObserver(this);
         }
 
-        if (!isFragmentHidden()) {
+        if (!isFragmentHidden() || !isOptimizationEnabled()) {
             if (getAnimation() != PresentAnimation.None) {
                 postponeEnterTransition();
             }
