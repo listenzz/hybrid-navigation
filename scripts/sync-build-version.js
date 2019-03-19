b8f655dfa5816730d9338e3b1bfc848d1a3dbf7a
Listen <listenzz@163.com>
2019-03-19 15:46:23 +0800

    add prettier rc back

--- scripts/sync-build-version.js
+++ scripts/sync-build-version.js
@@ -1,29 +1,19 @@
-const fs = require("fs-extra");
+const fs = require('fs-extra');
 
-const vectorIconsBuildGradle =
-  "./node_modules/react-native-vector-icons/android/build.gradle";
-const hud = "./node_modules/react-native-hud-hybrid/android/build.gradle";
+const vectorIconsBuildGradle = './node_modules/react-native-vector-icons/android/build.gradle';
+const hud = './node_modules/react-native-hud-hybrid/android/build.gradle';
 const gradles = [vectorIconsBuildGradle, hud];
 
 gradles.forEach(gradle => {
-  fs.readFile(gradle, "utf8", function(err, data) {
+  fs.readFile(gradle, 'utf8', function(err, data) {
     if (err) {
       console.warn(err);
       return;
     }
 
-    let str = data.replace(
-      /^(\s+compileSdkVersion).*$/gm,
-      "$1 rootProject.ext.compileSdkVersion"
-    );
-    str = str.replace(
-      /^(\s+buildToolsVersion).*$/gm,
-      "$1 rootProject.ext.buildToolsVersion"
-    );
-    str = str.replace(
-      /^(\s+targetSdkVersion).*$/gm,
-      "$1 rootProject.ext.targetSdkVersion"
-    );
+    let str = data.replace(/^(\s+compileSdkVersion).*$/gm, '$1 rootProject.ext.compileSdkVersion');
+    str = str.replace(/^(\s+buildToolsVersion).*$/gm, '$1 rootProject.ext.buildToolsVersion');
+    str = str.replace(/^(\s+targetSdkVersion).*$/gm, '$1 rootProject.ext.targetSdkVersion');
     str = str.replace(
       /["'](com\.android\.support:appcompat-v7:).*["']/gm,
       '"$1$rootProject.ext.supportLibVersion"'
@@ -36,13 +26,13 @@ gradles.forEach(gradle => {
       /["'](com\.android\.support:design:).*["']/gm,
       '"$1$rootProject.ext.supportLibVersion"'
     );
-    str = str.replace(/\scompile\s/gm, " implementation ");
+    str = str.replace(/\scompile\s/gm, ' implementation ');
     str = str.replace(
       /classpath\s+'com\.android\.tools\.build:gradle:.+['""]/gm,
       `classpath 'com.android.tools.build:gradle:3.3.2'`
     );
-    if (str.search("google\\(\\)") === -1) {
-      str = str.replace(/(.+)jcenter\(\)/gm, "$1jcenter()\n$1google()");
+    if (str.search('google\\(\\)') === -1) {
+      str = str.replace(/(.+)jcenter\(\)/gm, '$1jcenter()\n$1google()');
     }
     fs.outputFile(gradle, str);
   });
