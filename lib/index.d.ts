e885c44df7c5d51af9858fc82ec04eef0cced2f7
listen <listenzz@163.com>
2019-05-11 17:24:20 +0800

    some config

--- lib/index.d.ts
+++
@@ -1,9 +0,0 @@
-export * from './src/Navigator';
-export * from './src/Garden';
-export * from './src/ReactRegistry';
-export * from './src/router';
-declare const RESULT_OK: -1;
-declare const RESULT_CANCEL: 0;
-declare const DARK_CONTENT: "dark-content";
-declare const LIGHT_CONTENT: "light-content";
-export { RESULT_OK, RESULT_CANCEL, DARK_CONTENT, LIGHT_CONTENT };
--- lib/src/utils.d.ts
+++
@@ -1,9 +0,0 @@
-import { Navigator } from './Navigator';
-export interface BindOptions {
-    inLayout?: boolean;
-    sceneId?: string | undefined;
-    navigatorFactory?: (sceneId: string) => Navigator;
-}
-declare function bindBarButtonItemClickEvent(item?: {}, options?: BindOptions): void;
-declare function removeBarButtonItemClickEvent(sceneId: string): void;
-export { bindBarButtonItemClickEvent, removeBarButtonItemClickEvent };
