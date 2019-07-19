31ee185af2b1992cbe3a1c28ed925e837a1dcf2f
listen <listenzz@163.com>
2019-07-19 21:48:44 +0800

    remove badge border color

--- ios/NavigationHybrid/UITabBar+Badge.m
+++ ios/NavigationHybrid/UITabBar+Badge.m
@@ -6,7 +6,6 @@
 //  Copyright © 2018年 Listen. All rights reserved.
 //
 #import "UITabBar+Badge.h"
-#import "HBDGarden.h"
 
 @implementation UITabBar (DotBadge)
 
@@ -24,9 +23,9 @@
 
     CGFloat percentX = (index + 0.5) / self.items.count;
     CGFloat x = ceilf(percentX * self.frame.size.width + 10);
-    CGFloat y = 4;
+    CGFloat y = 5;
     //10为小红点的高度和宽度
-    label.frame = CGRectMake(x, y, 14, 14);
+    label.frame = CGRectMake(x, y, 10, 10);
     [self addSubview:label];
     //把小红点移到最顶层
     [self bringSubviewToFront:label];
@@ -44,43 +43,15 @@
 
 - (void)styleLabel:(UILabel *)label {
     CGRect rect = label.bounds;
-    
-    CGFloat radius = 7;
-    CGFloat borderWidth = 4;
-    UIColor *borderColor = [HBDGarden globalStyle].tabBarBackgroundColor ? [HBDGarden globalStyle].tabBarBackgroundColor : [UIColor whiteColor];
-    
-    //Make round
-    // Create the path for to make circle
+    CGFloat radius = 5;
     UIBezierPath *maskPath = [UIBezierPath bezierPathWithRoundedRect:rect
                                                    byRoundingCorners:UIRectCornerAllCorners
                                                          cornerRadii:CGSizeMake(radius, radius)];
-    
-    // Create the shape layer and set its path
+
     CAShapeLayer *maskLayer = [CAShapeLayer layer];
-    
     maskLayer.frame = rect;
     maskLayer.path  = maskPath.CGPath;
-    
-    // Set the newly created shape layer as the mask for the view's layer
     label.layer.mask = maskLayer;
-    
-    //Give Border
-    //Create path for border
-    UIBezierPath *borderPath = [UIBezierPath bezierPathWithRoundedRect:rect
-                                                     byRoundingCorners:UIRectCornerAllCorners
-                                                           cornerRadii:CGSizeMake(radius, radius)];
-    
-    // Create the shape layer and set its path
-    CAShapeLayer *borderLayer = [CAShapeLayer layer];
-    
-    borderLayer.frame       = rect;
-    borderLayer.path        = borderPath.CGPath;
-    borderLayer.strokeColor = borderColor.CGColor;
-    borderLayer.fillColor   = [UIColor clearColor].CGColor;
-    borderLayer.lineWidth   = borderWidth;
-    
-    //Add this layer to give border.
-    [[label layer] addSublayer:borderLayer];
 }
 
 @end
