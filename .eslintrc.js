13e9773afa99bf2225a3880031a0066dbeb98ef0
listen <listenzz@163.com>
2020-06-08 11:55:32 +0800

    bump version

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,8 @@
 module.exports = {
   root: true,
-  extends: ['@gfez/react-native', 'plugin:prettier/recommended', 'prettier/react']
+  extends: ['@gfez/react-native', 'plugin:prettier/recommended', 'prettier/react'],
+  rules: {
+    'no-shadow': 0,
+    'react-native/no-inline-styles': 0,
+  },
 };
