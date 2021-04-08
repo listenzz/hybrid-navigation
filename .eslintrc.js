b6aa1a163a81ffa0ea8f9b2987508e4bba20ffb4
listen <listenzz@163.com>
2021-04-08 17:51:56 +0800

    更正 eslint 配置

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,6 @@
 module.exports = {
   root: true,
-  extends: ['@react-native-community', 'plugin:prettier/recommended', 'prettier/react'],
+  extends: ['@gfez/react-native', 'plugin:prettier/recommended', 'prettier/react'],
   overrides: [
     {
       files: ['jest/*'],
