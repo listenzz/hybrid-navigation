cb4c77e5299404f55bef10bea24a945c585c5e32
listen <listenzz@163.com>
2021-05-31 15:11:55 +0800

    处理 stack 路由的 child 可能不是 screen 的问题

--- src/router/stack.ts
+++ src/router/stack.ts
@@ -1,4 +1,4 @@
-import { RouteGraph, LayoutMode, RouteInfo } from './typing'
+import { RouteGraph, LayoutMode, RouteInfo, RouteHandler } from './typing'
 import { isScreenGraph } from './screen'
 import { Navigator } from '../Navigator'
 
@@ -13,49 +13,65 @@ export function isStackGraph(graph: RouteGraph): graph is StackGraph {
   return graph.layout === 'stack'
 }
 
-export function stackRouteHandler(graph: RouteGraph, route: RouteInfo) {
+export function stackRouteHandler(graph: RouteGraph, route: RouteInfo, next: RouteHandler) {
   if (!isStackGraph(graph)) {
     return false
   }
 
-  const { children } = graph
   const { mode, moduleName, dependencies, props } = route
 
-  if (mode === 'push') {
-    let moduleNames = [...dependencies, moduleName]
-    let index = -1
+  if (mode !== 'push') {
+    return false
+  }
 
-    for (let i = children.length - 1; i > -1; i--) {
-      const child = children[i]
-      if (isScreenGraph(child)) {
-        index = moduleNames.indexOf(child.moduleName)
-        if (index !== -1) {
-          break
-        }
-      }
-    }
+  const { children } = graph
+  const expectedModuleNames = [...dependencies, moduleName]
+  let index = -1
 
+  for (let i = children.length - 1; i > -1; i--) {
+    const existingModuleNames: string[] = []
+    extractModuleNames(children[i], existingModuleNames)
+    index = expectedModuleNames.findIndex(name => existingModuleNames.includes(name))
     if (index !== -1) {
-      let peddingModuleNames = moduleNames.slice(index + 1)
-      const navigator = Navigator.of(children[children.length - 1].sceneId)
-      if (peddingModuleNames.length === 0) {
-        if (JSON.stringify(props) === '{}') {
-          navigator.popTo(moduleName)
-        } else {
-          navigator.redirectTo(moduleName, props)
-        }
-      } else {
-        for (let i = 0; i < peddingModuleNames.length; i++) {
-          if (i === peddingModuleNames.length - 1) {
-            navigator.push(moduleName, props)
-          } else {
-            navigator.push(peddingModuleNames[i], {}, {})
-          }
-        }
-      }
-      return true
+      break
     }
   }
 
+  if (index !== -1) {
+    let pendingModuleNames = expectedModuleNames.slice(index + 1)
+    const navigator = Navigator.of(children[children.length - 1].sceneId)
+    if (!isScreenGraph(children[index])) {
+      next(children[index], route, next)
+    }
+
+    if (pendingModuleNames.length === 0) {
+      if (JSON.stringify(props) === '{}') {
+        navigator.popTo(moduleName)
+      } else {
+        navigator.redirectTo(moduleName, props)
+      }
+    } else {
+      for (let i = 0; i < pendingModuleNames.length; i++) {
+        if (i === pendingModuleNames.length - 1) {
+          navigator.push(moduleName, props)
+        } else {
+          navigator.push(pendingModuleNames[i], {}, {})
+        }
+      }
+    }
+    return true
+  }
+
   return false
+}
+
+function extractModuleNames(graph: RouteGraph, set: string[]) {
+  if (isScreenGraph(graph)) {
+    set.push(graph.moduleName)
+  } else {
+    const children = graph.children!
+    for (let i = 0; i < children.length; i++) {
+      extractModuleNames(children[i], set)
+    }
+  }
 }
