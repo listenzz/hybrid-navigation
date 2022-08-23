4858e30f59049a7d40430158ca070d1b34f7b857
listen <listenzz@163.com>
2022-08-23 16:13:49 +0800

    更新 editorconfig, 设置参考线

--- .eslintrc.js
+++ .eslintrc.js
@@ -10,7 +10,10 @@ module.exports = {
     },
   ],
   rules: {
-    '@typescript-eslint/no-unused-vars': ['warn', { argsIgnorePattern: '^_', varsIgnorePattern: '^_' }],
+    '@typescript-eslint/no-unused-vars': [
+      'warn',
+      { argsIgnorePattern: '^_', varsIgnorePattern: '^_' },
+    ],
     'no-unused-vars': 'off',
   },
 }
