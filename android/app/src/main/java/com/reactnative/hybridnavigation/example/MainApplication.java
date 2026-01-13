56cc97f87c40a6781244f36624c916a72116a600
listen <listenzz@163.com>
2026-01-14 01:00:55 +0800

    优化构建配置

--- android/app/src/main/java/com/reactnative/hybridnavigation/example/MainApplication.java
+++ android/app/src/main/java/com/reactnative/hybridnavigation/example/MainApplication.java
@@ -9,16 +9,13 @@ import com.facebook.common.logging.FLog;
 import com.facebook.react.PackageList;
 import com.facebook.react.ReactApplication;
 import com.facebook.react.ReactHost;
+import com.facebook.react.ReactNativeApplicationEntryPoint;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
-import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint;
 import com.facebook.react.defaults.DefaultReactHost;
 import com.facebook.react.defaults.DefaultReactNativeHost;
-import com.facebook.react.soloader.OpenSourceMergedSoMapping;
-import com.facebook.soloader.SoLoader;
 import com.reactnative.hybridnavigation.ReactManager;
 
-import java.io.IOException;
 import java.util.List;
 
 public class MainApplication extends Application implements ReactApplication {
@@ -61,15 +58,7 @@ public class MainApplication extends Application implements ReactApplication {
     @Override
     public void onCreate() {
         super.onCreate();
-        try {
-            SoLoader.init(this, OpenSourceMergedSoMapping.INSTANCE);
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-        if (BuildConfig.IS_NEW_ARCHITECTURE_ENABLED) {
-            // If you opted-in for the New Architecture, we load the native entry point for this app.
-            DefaultNewArchitectureEntryPoint.load();
-        }
+		ReactNativeApplicationEntryPoint.loadReactNative(this);
 
         ReactManager reactManager = ReactManager.get();
         reactManager.install(getReactHost());
--- packages/navigation/HybridNavigation.podspec
+++ packages/navigation/HybridNavigation.podspec
@@ -13,7 +13,7 @@ Pod::Spec.new do |s|
   s.platforms    = { :ios => min_ios_version_supported }
   s.source       = { :git => "https://github.com/listenzz/hybrid-navigation.git", :tag => "#{s.version}" }
 
-  s.source_files = "ios/HybridNavigation/**/*.{h,m,mm,cpp}"
+  s.source_files = "ios/**/*.{h,m,mm,cpp}"
   # s.private_header_files = "ios/HybridNavigation/**/*.h"
 
   install_modules_dependencies(s)
